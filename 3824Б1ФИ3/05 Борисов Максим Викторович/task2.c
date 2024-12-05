48a1db138525a049cacd96259b3c4ed25754e12d
MaksimBorisov1 <hashiramagg8877@gmail.com>
2024-12-05 10:23:37 +0300

    Update task2.c

--- 3824Б1ФИ3/05 Борисов Максим Викторович/task2.c
+++ 3824Б1ФИ3/05 Борисов Максим Викторович/task2.c
@@ -2,5 +2,25 @@
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
+ if (a == 0 || b == 0) {
+return 0;
+} else if (UINT_MAX / a < b) {
+return UINT_MAX;
+} else {
+return a * b;
+}
+}
+
+int main()
+{
+unsigned int a, b;
+
+printf("Введите два натуральных числа a и b: ");
+scanf("%u %u", &a, &b);
+
+unsigned int result = product_of_two_numbers(a, b);
+
+printf("Произведение чисел a и b: %u\n", result);
+
   return 0;
 }
