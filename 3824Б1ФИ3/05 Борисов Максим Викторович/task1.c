19166185d5a3e786a4ac946d58484922d150e4e6
MaksimBorisov1 <hashiramagg8877@gmail.com>
2024-12-05 10:19:06 +0300

    Update task1.c

--- 3824Б1ФИ3/05 Борисов Максим Викторович/task1.c
+++ 3824Б1ФИ3/05 Борисов Максим Викторович/task1.c
@@ -3,5 +3,11 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
+   if (a >= 0 && b >= 0) {
+    if (a > INT_MAX - b) return INT_MAX;
+  } else if (a < 0 && b < 0) {
+    if (a < INT_MIN - b) return INT_MIN;
+  }
+  return a + b;
   return 0;
 }
